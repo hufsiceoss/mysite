af3af94c40cf0abca33b6d86fed3a3ac9ab9eda3
jinyoung moon <mjy9650@naver.com>
2022-04-04 17:35:36 +0900

    first

---
+++ pybo/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.index, name='index'),
+    path('use/', views.use, name='use'),
+    #path('use/', views.click),
+    path('use/<int:id>', views.image, name='image')
+]
---
+++ pybo/views.py
@@ -0,0 +1,71 @@
+from django.shortcuts import render
+from .models import Image
+from django.http import HttpResponse
+
+from account.models import Account
+
+# 참고
+# https://gosmcom.tistory.com/143 : 로그인 처리 이름 불러오기
+# https://free-eunb.tistory.com/43 : 이미지 for 문처리
+
+def index(request):
+    #return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.!")
+    userstate = request.session.get('user')
+    name = {}
+
+    if userstate: #현재 로그인 상태이면! 이름을 출력할 수 있게끔
+        account = Account.objects.get(pk=userstate)
+        #name['name'] = '문진영'
+        name['name'] = account.username
+
+        return render(request, 'main.html', name)
+
+    else:
+        return render(request, 'main.html')
+
+def use(request):
+    userstate = request.session.get('user')
+    account = Account.objects.get(pk=userstate)
+    name = account.username
+
+    if 'imgUpload' in request.method == "POST":
+        img = request.FILES.get('image', None)
+
+        if not img:
+            #에러메시지 출력해줬음 좋겠음 이미지를 선택해주세요~~뭐 이런거 일단은 새로고침 형태로 이동
+            return render(request, 'use.html', {'name':name})
+
+        else:
+            image = Image(
+                #idx = request.session.get('user'),
+                email=account.useremail,
+                image = img,
+            )
+
+            image.save()
+
+            #imgList = Image.objects.filter(email=account.useremail)
+            #return HttpResponse(Image.image)
+        imgList = Image.objects.filter(email=account.useremail)
+        return render(request, 'use.html', {'name': name, 'imgList': imgList})
+
+    else:
+        imgList = Image.objects.filter(email=account.useremail)
+
+        return render(request, 'use.html', {'name': name, 'imgList': imgList})
+        #return render(request, 'use.html', {'name': name})
+
+
+def image(request, id):
+    userstate = request.session.get('user')
+    account = Account.objects.get(pk=userstate)
+    name = account.username
+
+    imgList = Image.objects.filter(email=account.useremail)
+
+    img_info = Image.objects.get(pk=id)
+
+    img_rcs = img_info.image
+
+
+    return render(request, 'use.html', {'name':name, 'imgList':imgList, 'img_rcs':img_rcs})
